032bb1bb34305961c4392fe4084706d5cdb8a044
PatrickPD01 <61233451+PatrickPD01@users.noreply.github.com>
2021-06-10 22:43:56 +0200

    Add files via upload

---
+++ Kaffemaskinen Del 1/Kaffemaskinen/Ingredient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffemaskinen
+{
+
+    class IngredientContainer
+    {
+        // The ingredients for the coffee machine
+        public enum ingredients { 
+            coffee,
+            tee,
+        }
+
+        public string FillIngredients(ingredients ingredient)
+        {
+            if (ingredient.Equals(ingredients.coffee))
+            {
+                return "Filling inrgredient container with coffee";
+            }
+            else
+            {
+                return "Invalid ingredient inserted";
+            }
+        }
+
+        public enum ingredientsTee { tee, }
+
+        public string FillIngredientsTee(ingredients ingredient)
+        {
+            if (ingredient.Equals(ingredients.tee))
+            {
+                return "Boiling water for tee";
+            }
+            else
+            {
+                return "Making tee";
+            }
+        }
+
+
+    }
+}
